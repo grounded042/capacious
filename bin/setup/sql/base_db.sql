f01ef82080c7ea0bcbd9ace4873bd58e02b72692
Jon Carl <joncarl42@gmail.com>
2015-09-19 11:17:23 -0400

    add the invitees table creation to setup

--- bin/setup/sql/base_db.sql
+++ bin/setup/sql/base_db.sql
@@ -11,3 +11,16 @@ CREATE TABLE events (
     created_at timestamp default current_timestamp,
     updated_at timestamp default current_timestamp
 );
+
+CREATE TABLE invitees (
+	invitee_id uuid DEFAULT uuid_generate_v1mc() PRIMARY KEY,
+	fk_event_id uuid REFERENCES events (event_id),
+	first_name varchar(255) NOT NULL,
+	last_name varchar(255) NOT NULL,
+	email varchar(255) NOT NULL,
+	date_attending boolean,
+	date_first_name varchar(255),
+	date_last_name varchar(255),
+	created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp
+);
