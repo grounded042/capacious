9206072b169d6a32870814abbe2f9530c65bc2db
Jon Carl <joncarl42@gmail.com>
2015-09-19 11:23:05 -0400

    add invitees seed data

--- bin/setup/sql/seed_dev_data.sql
+++ bin/setup/sql/seed_dev_data.sql
@@ -13,5 +13,11 @@ BEGIN
         'cd7bc650-2e71-11e5-a390-675459d99309', 'Picnic', 'Your normal picnic.', '2015-12-15 17:00:00.000000',
         '2015-12-15 22:00:00.000000', '2015-07-11 22:36:31.024391', '2015-07-11 22:36:31.024391'
     );
-    
+
+    INSERT INTO invitees(invitee_id, fk_event_id, first_name, last_name, email)
+    VALUES (
+    	'24669e54-5ee2-11e5-a379-7b2796b289b2', 'cd7bc650-2e71-11e5-a390-675459d99309', 'Saxton', 'Hale', 
+    	'shale@mann.co'
+	);
+
 END $$
