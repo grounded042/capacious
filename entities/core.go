b0b01e4a7f42147831746e962661d8510bbf1b19
Jon Carl <joncarl42@gmail.com>
2015-09-19 15:38:41 -0400

    add Invitee and Date structs

--- entities/core.go
+++ entities/core.go
@@ -11,3 +11,24 @@ type Event struct {
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
 }
+
+type Invitee struct {
+	InviteeId string    `gorm:"primary_key" sql:"DEFAULT:uuid_generate_v1mc()" json:"invitee_id"`
+	FkEventId string    `json:"-"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	Date      Date      `json:"date"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type Date struct {
+	DateId      string    `gorm:"primary_key" sql:"DEFAULT:uuid_generate_v1mc()" json:"date_id"`
+	FkInviteeId string    `json:"-"`
+	FirstName   string    `json:"first_name"`
+	LastName    string    `json:"last_name"`
+	Attending   bool      `json:"attending"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
