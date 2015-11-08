4bf9d53d71dcc7a93248fc20e061e8a0ecda3475
Jon Carl <joncarl42@gmail.com>
2015-11-08 14:40:05 -0500

    add support for menu options
    closes #3

--- bin/setup/sql/base_db.sql
+++ bin/setup/sql/base_db.sql
@@ -3,41 +3,59 @@ CREATE EXTENSION "uuid-ossp";
 
 -- create the tables
 CREATE TABLE events (
-    event_id uuid DEFAULT uuid_generate_v1mc() PRIMARY KEY,
-    name varchar(255) NOT NULL UNIQUE,
-    description varchar(255) NOT NULL,
-    location varchar(255) NOT NULL,
-    start_time timestamp,
-    end_time timestamp,
-    allowed_friends int,
-    created_at timestamp default current_timestamp,
-    updated_at timestamp default current_timestamp
+  event_id uuid DEFAULT uuid_generate_v1mc() PRIMARY KEY,
+  name varchar(255) NOT NULL UNIQUE,
+  description varchar(255) NOT NULL,
+  location varchar(255) NOT NULL,
+  start_time timestamp,
+  end_time timestamp,
+  allowed_friends int,
+  created_at timestamp default current_timestamp,
+  updated_at timestamp default current_timestamp
 );
 
 CREATE TABLE guests (
-	guest_id uuid DEFAULT uuid_generate_v1mc() PRIMARY KEY,
-	first_name varchar(255) NOT NULL,
-	last_name varchar(255) NOT NULL,
-	attending boolean DEFAULT false,
-	created_at timestamp default current_timestamp,
+  guest_id uuid DEFAULT uuid_generate_v1mc() PRIMARY KEY,
+  first_name varchar(255) NOT NULL,
+  last_name varchar(255) NOT NULL,
+  attending boolean DEFAULT false,
+  created_at timestamp default current_timestamp,
     updated_at timestamp default current_timestamp
 );
 
 CREATE TABLE invitees (
-	invitee_id uuid DEFAULT uuid_generate_v1mc() PRIMARY KEY,
-	fk_event_id uuid REFERENCES events (event_id),
-	fk_guest_id uuid REFERENCES guests (guest_id),
-	email varchar(255) NOT NULL UNIQUE,
-	created_at timestamp default current_timestamp,
-    updated_at timestamp default current_timestamp
+  invitee_id uuid DEFAULT uuid_generate_v1mc() PRIMARY KEY,
+  fk_event_id uuid REFERENCES events (event_id),
+  fk_guest_id uuid REFERENCES guests (guest_id),
+  email varchar(255) NOT NULL UNIQUE,
+  created_at timestamp default current_timestamp,
+  updated_at timestamp default current_timestamp
 );
 
 CREATE TABLE invitee_friends (
-	invitee_friend_id uuid DEFAULT uuid_generate_v1mc() PRIMARY KEY,
-	fk_invitee_id uuid REFERENCES invitees (invitee_id),
-	fk_guest_id uuid REFERENCES guests (guest_id),
-	created_at timestamp default current_timestamp,
-    updated_at timestamp default current_timestamp
+  invitee_friend_id uuid DEFAULT uuid_generate_v1mc() PRIMARY KEY,
+  fk_invitee_id uuid REFERENCES invitees (invitee_id),
+  fk_guest_id uuid REFERENCES guests (guest_id),
+  created_at timestamp default current_timestamp,
+  updated_at timestamp default current_timestamp
 );
 
+CREATE TABLE menu_items (
+  menu_item_id uuid DEFAULT uuid_generate_v1mc() PRIMARY KEY,
+  fk_event_id uuid REFERENCES events (event_id),
+  item_order int,
+  name varchar(255),
+  num_choices int,
+  created_at timestamp default current_timestamp,
+  updated_at timestamp default current_timestamp,
+  UNIQUE (fk_event_id, item_order)
+);
 
+CREATE TABLE menu_item_options (
+  menu_item_option_id uuid DEFAULT uuid_generate_v1mc() PRIMARY KEY,
+  fk_menu_item_id uuid REFERENCES menu_items (menu_item_id),
+  name varchar(255),
+  description varchar(255),
+  created_at timestamp default current_timestamp,
+  updated_at timestamp default current_timestamp
+);
--- bin/setup/sql/seed_dev_data.sql
+++ bin/setup/sql/seed_dev_data.sql
@@ -7,12 +7,69 @@ DECLARE
     uid uuid;
 BEGIN
 
+    -- create a test event
     INSERT INTO events(event_id, name, description, location,
-        start_time, end_time, created_at, updated_at)
+        start_time, end_time, allowed_friends, created_at, updated_at)
     VALUES (
-        'cd7bc650-2e71-11e5-a390-675459d99309', 'Picnic', 'Your normal picnic.', 'The Park', 
-        '2015-12-15 17:00:00.000000', '2015-12-15 22:00:00.000000', '2015-07-11 22:36:31.024391', 
+        'cd7bc650-2e71-11e5-a390-675459d99309', 'Picnic', 'Your normal picnic.', 'The Park',
+        '2015-12-15 17:00:00.000000', '2015-12-15 22:00:00.000000', 2, '2015-07-11 22:36:31.024391',
         '2015-07-11 22:36:31.024391'
+    );
+
+    -- create menu options for the test event
+    INSERT INTO menu_items(menu_item_id, fk_event_id, item_order, name, num_choices)
+    VALUES (
+      'F167EB18-864E-11E5-A016-6B70107C9BC3', 'cd7bc650-2e71-11e5-a390-675459d99309',
+      1, 'Snacks', 2
+    );
+
+    INSERT INTO menu_items(menu_item_id, fk_event_id, item_order, name, num_choices)
+    VALUES (
+      'F1680616-864E-11E5-A016-63F8FBFFDC49', 'cd7bc650-2e71-11e5-a390-675459d99309',
+      2, 'Sandwich', 1
+    );
+
+    INSERT INTO menu_items(menu_item_id, fk_event_id, item_order, name, num_choices)
+    VALUES (
+      'F1680AC6-864E-11E5-A016-CB0185CDAD5A', 'cd7bc650-2e71-11e5-a390-675459d99309',
+      3, 'Dessert', 1
+    );
+
+    INSERT INTO menu_item_options(fk_menu_item_id, name, description)
+    VALUES (
+      'F167EB18-864E-11E5-A016-6B70107C9BC3', 'Cheese & Crackers',
+      'Your typical cheese and crackers snack.'
+    );
+
+    INSERT INTO menu_item_options(fk_menu_item_id, name, description)
+    VALUES (
+      'F167EB18-864E-11E5-A016-6B70107C9BC3', 'Pretzels', 'See name.'
+    );
+
+    INSERT INTO menu_item_options(fk_menu_item_id, name, description)
+    VALUES (
+      'F167EB18-864E-11E5-A016-6B70107C9BC3', 'Graham Crackers',
+      'A cracker made of graham.'
+    );
+
+    INSERT INTO menu_item_options(fk_menu_item_id, name, description)
+    VALUES (
+      'F1680616-864E-11E5-A016-63F8FBFFDC49', 'BLT', 'Bacon, lettuce, and tomato. A classic.'
+    );
+
+    INSERT INTO menu_item_options(fk_menu_item_id, name, description)
+    VALUES (
+      'F1680616-864E-11E5-A016-63F8FBFFDC49', 'Grilled Cheese', 'You cannnot go wrong.'
+    );
+
+    INSERT INTO menu_item_options(fk_menu_item_id, name, description)
+    VALUES (
+      'F1680AC6-864E-11E5-A016-CB0185CDAD5A', 'Brownies', 'Moist and delicious.'
+    );
+
+    INSERT INTO menu_item_options(fk_menu_item_id, name, description)
+    VALUES (
+      'F1680AC6-864E-11E5-A016-CB0185CDAD5A', 'Chocolate Chip Cookies', 'Gooey and good.'
     );
 
     INSERT INTO guests(guest_id, first_name, last_name, attending)
@@ -34,7 +91,7 @@ BEGIN
 
     INSERT INTO invitee_friends(invitee_friend_id, fk_invitee_id, fk_guest_id)
     VALUES (
-        'e6afb5b0-7b64-11e5-b861-1f0fc9657754', 'fb3c11f8-7917-11e5-8b8e-b3a0b1b9b068', 
+        'e6afb5b0-7b64-11e5-b861-1f0fc9657754', 'fb3c11f8-7917-11e5-8b8e-b3a0b1b9b068',
         '81e6d338-7917-11e5-8b8e-a37beb0fdab8'
     );
 
