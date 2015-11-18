417cc06656907ec71e2a1432014bec0eabb4a73f
Jon Carl <joncarl42@gmail.com>
2015-11-18 15:46:42 -0500

    disable methods that should be behind auth

--- routes/events.go
+++ routes/events.go
@@ -4,31 +4,31 @@ import "github.com/grounded042/capacious/controllers"
 
 func EventRoutes(cl controllers.List) []Route {
 	return []Route{
-		Route{
-			Method:  "get",
-			Pattern: "/events",
-			Handler: cl.Events.GetEvents,
-		},
-		Route{
-			Method:  "post",
-			Pattern: "/events",
-			Handler: cl.Events.CreateEvent,
-		},
+		// Route{
+		// 	Method:  "get",
+		// 	Pattern: "/events",
+		// 	Handler: cl.Events.GetEvents,
+		// },
+		// Route{
+		// 	Method:  "post",
+		// 	Pattern: "/events",
+		// 	Handler: cl.Events.CreateEvent,
+		// },
 		Route{
 			Method:  "get",
 			Pattern: "/events/:id",
 			Handler: cl.Events.GetEventInfo,
 		},
-		Route{
-			Method:  "get",
-			Pattern: "/events/:id/relationships/invitees",
-			Handler: cl.Invitees.GetInviteesForEvent,
-		},
-		Route{
-			Method:  "post",
-			Pattern: "/events/:id/relationships/invitees",
-			Handler: cl.Invitees.CreateInviteeForEvent,
-		},
+		// Route{
+		// 	Method:  "get",
+		// 	Pattern: "/events/:id/relationships/invitees",
+		// 	Handler: cl.Invitees.GetInviteesForEvent,
+		// },
+		// Route{
+		// 	Method:  "post",
+		// 	Pattern: "/events/:id/relationships/invitees",
+		// 	Handler: cl.Invitees.CreateInviteeForEvent,
+		// },
 		Route{
 			Method:  "get",
 			Pattern: "/events/:id/relationships/menu_items",
